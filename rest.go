5818a3a5bca4b808c299121e3911438bfc108fc9
amnonbb <amnonbb@gmail.com>
2019-11-30 15:53:00 +0200

    Parse json and events in handleEvent

--- rest.go
+++ rest.go
@@ -40,7 +40,7 @@ func (a *App) handleProtocol(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Write to log
-	err := writeToLog(os.Getenv(EnvLogPath) + "/protocol.log", string(b))
+	err := writeToLog(os.Getenv(EnvLogPath)+"/protocol.log", string(b))
 	if err != nil {
 		fmt.Println("Log Failed:", err)
 	}
@@ -52,11 +52,38 @@ func (a *App) handleProtocol(w http.ResponseWriter, r *http.Request) {
 func (a *App) handelEvent(w http.ResponseWriter, r *http.Request) {
 	b, _ := ioutil.ReadAll(r.Body)
 
-	err := writeToLog(os.Getenv(EnvLogPath) + "/events.log", string(b))
-	if err != nil {
-		fmt.Println("Log Failed:", err)
+	// Parse JSON
+	var data map[string]interface{}
+	json.Unmarshal([]byte(b), &data)
+	msg := data["event"].(map[string]interface{})
+	plugin := msg["plugin"].(string)
+
+	// Filter plugin events
+	if plugin == "janus.plugin.videoroom" {
+
+		event_data := msg["data"].(map[string]interface{})
+
+		// Check if property exist
+		if _, ok := event_data["event"].(string); ok {
+
+			user_data := event_data["display"].(string)
+			var user map[string]interface{}
+			json.Unmarshal([]byte(user_data), &user)
+			//u, _ := json.Marshal(user)
+
+			// Filter data event
+			switch t := event_data["event"].(string); t {
+			case "joined", "leaving":
+				err := writeToLog(os.Getenv(EnvLogPath)+"/events.log", string(b))
+				if err != nil {
+					fmt.Println("Log Failed:", err)
+				}
+				defer r.Body.Close()
+			default:
+				fmt.Println("no cases in switch")
+			}
+		}
 	}
-	defer r.Body.Close()
 
 	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
